81137d7f87d61016ac2bfa6518fefaff372a68be
Omurbek <shaiurbekuuluomurbek@gmail.com>
2025-01-31 12:59:04 +0600

    Commit

---
+++ src/main/java/onlainosago/OnlineOsagoApplication.java
@@ -0,0 +1,13 @@
+package onlainosago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOsagoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineOsagoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/onlainosago/OnlineOsagoApplicationTests.java
@@ -0,0 +1,13 @@
+package onlainosago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineOsagoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
